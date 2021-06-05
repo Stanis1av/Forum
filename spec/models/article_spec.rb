11f23ba021cb0b33e22d94ec876e3d70642510b2
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-05 17:15:38 +0300

    Creating test for Article and Comment

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should have_many(:comments).dependent(:destroy) }
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:id_author) }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  # Association test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:article) }
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:author) }
+  it { should validate_presence_of(:id_author) }
 end
