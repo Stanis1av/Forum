a7d44fc8a3c8470d88dac195f9776ee902333270
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-05 23:57:45 +0300

    Updating rails helper and adding requests folder to the spec directory with the corresponding spec files.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,7 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -32,6 +33,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+  # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -64,32 +76,22 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  # configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+  # add `FactoryBot` methods
+  config.include FactoryBot::Syntax::Methods
+
+  # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
   end
 
+  # start the transaction strategy as examples are run
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
   # [...]
-  RSpec.configure do |config|
-    # [...]
-    # add `FactoryBot` methods
-    config.include FactoryBot::Syntax::Methods
 
-    # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
-    config.before(:suite) do
-      DatabaseCleaner.clean_with(:truncation)
-      DatabaseCleaner.strategy = :transaction
-    end
-
-    # start the transaction strategy as examples are run
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
-    end
-    # [...]
-  end
+  config.include RequestSpecHelper, type: :request
 end
