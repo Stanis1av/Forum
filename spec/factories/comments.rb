e5fa84a615dbfe15efc3508e4b50dcecb8e42717
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-14 14:58:15 +0300

    Creating Comments controller and Comments Spec

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :comment do
     body { Faker::Lorem.paragraph(sentence_count: rand(1..5)) }
     author {Faker::Name.name }
-    user_id { nil }
+    user_id { Faker::Number.non_zero_digit }
     article_id { nil } # {Faker::Number.between(from: (Article.first.id), to: (Article.last.id))}
   end
 end
