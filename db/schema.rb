db96e50e357affaea4554cf12b4b57589d5ee27b
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-17 04:07:54 +0300

    Creating controller, model, views and migration for Article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_172931) do
+ActiveRecord::Schema.define(version: 2021_06_17_003820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
