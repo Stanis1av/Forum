ff364764c2f8653f26f4bc313ea2bcc57bd51ef3
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-18 13:03:19 +0300

    Updating controller and views Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
+
+  has_many :comments # связываем две сущности между собой
   belongs_to :user
 end
