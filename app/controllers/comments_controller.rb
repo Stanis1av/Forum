767e3d51a521bd4373db51832e6548ddd041c481
Stanis1av <krasavin.stanislaus@gmail.com>
2021-06-18 16:51:21 +0300

    Final changes to models, controllers and views for Article and Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,18 +2,27 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!, :only => [:create]
 
   def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.new(comment_params)
+    # method 1
+    @comment = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
 
-    @comment.author_name = current_user.usename
+    @comment.author_name = current_user.username
 
     @comment.save
 
-    redirect_to article_path(@article)
+    redirect_to article_path(@comment.article)
+    # method 2
+    # @article = Article.find(params[:article_id])
+    # @comment = @article.comments.new(comment_params)
+
+    # @comment.author_name = current_user.username
+
+    # @comment.save
+
+    # redirect_to article_path(@article)
   end
 
   private
-
   def comment_params
     params.require(:comment).permit(:body)
   end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   validates :title, presence: true
-  validates :text, presence: true
+  validates :content, presence: true
 
   has_many :comments # связываем две сущности между собой
   belongs_to :user
